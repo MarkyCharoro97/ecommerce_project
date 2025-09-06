4a0c1cc5162b44ae81bb0bea1b0d108001f669c4
Mclaughlin Fanwell Charoro <102194625+MarkyCharoro97@users.noreply.github.com>
2025-09-06 13:52:27 +0200

    Delete ecommerce directory

--- ecommerce/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import Store, Product, Review, Order, OrderItem, ResetToken
-
-admin.site.register(Store)
-admin.site.register(Product)
-admin.site.register(Review)
-admin.site.register(Order)
-admin.site.register(OrderItem)
-admin.site.register(ResetToken)
